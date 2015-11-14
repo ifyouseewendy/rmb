c63be32b745cf51a2aaefa761703fe973c37abff
wendi <ifyouseewendy@gmail.com>
2015-11-14 20:53:02 +0800

    Add decimal support

--- lib/rmb_chinese_yuan.rb
+++ lib/rmb_chinese_yuan.rb
@@ -9,7 +9,7 @@ class RMB
   NUMBERS       = %w(零 壹 贰 叁 肆 伍 陆 柒 捌 玖)
   PART_UNIT     = %w(仟 佰 拾)
   INTEGER_UNIT  = %w(亿 万 元)
-  DECIMAL_UNIT  = %w(角 分)
+  DECIMAL_UNIT  = %w(整 角 分)
 
   attr_reader :integer, :decimal
 
@@ -28,20 +28,21 @@ class RMB
       money = money.to_s
 
       raise ArgumentError, "Not a valid money<#{money}>" \
-        unless money.match(/^[\d|,|\.]*/)
+        unless money.match(/^[\d|,|\.]+/)
 
       integer, decimal = money.gsub(',', '').split('.')
+      decimal = decimal ? decimal[0,2] : ''
 
       raise ArgumentError, "Integer part of money<#{money}> is invalid" \
-        unless integer.match(/^\d+$/)
+        unless integer.match(/^\d*$/)
 
       raise ArgumentError, "Decimal part of money<#{money}> is invalid" \
-        unless decimal.to_s.match(/^\d*$/)
+        unless decimal.match(/^\d*$/)
 
       raise ArgumentError, "Integer part of money<#{money}> is longer than 12" \
         if integer.length > 12
 
-      [integer, decimal].map(&:to_i)
+      [integer, decimal]
     end
 
     def convert_integer
@@ -51,19 +52,38 @@ class RMB
     end
 
     def convert_decimal
-      ''
+      return DECIMAL_UNIT[0] if @decimal.to_i.zero?
+
+      jiao, fen = @decimal.chars.map(&:to_i)
+      res = ''
+
+      if jiao.zero? && fen.to_i.nonzero?
+        res << NUMBERS[jiao]
+        res << [ NUMBERS[fen], DECIMAL_UNIT[2] ].join
+      else
+        res << [ NUMBERS[jiao], DECIMAL_UNIT[1] ].join
+        res << [ NUMBERS[fen],  DECIMAL_UNIT[2] ].join if fen && fen.nonzero?
+      end
+
+      res
     end
 
-    def join(integer, decimal)
-      [integer, decimal].join
+    def join(integer_words, decimal_words)
+      if integer.to_i.zero? && decimal.to_i.nonzero?
+        decimal_words
+      else
+        [integer_words, decimal_words].join
+      end
     end
 
     # Split money into three parts, each part is under 9999
     def split_into_parts
+      number = integer.to_i
+
       @parts = [
-        integer / 10**8,
-        (integer / 10**4) % 10**4,
-        integer % 10**4
+        number / 10**8,
+        (number / 10**4) % 10**4,
+        number % 10**4
       ]
     end
 
