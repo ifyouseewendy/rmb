4f8b1c05b2590bddb4d8e4a67ee1b2f131190165
Grayson Chen <cgg5207@sina.com>
2017-05-10 16:25:49 +0800

    remove CodeClimate::TestReporter.start

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+# CodeClimate::TestReporter.start
 require 'bigdecimal'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
